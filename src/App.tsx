7255a3d83a0ebc297fb2895a267b1cd863b5ddda
christinesy.tang <christinesy.tang@ap.jll.com>
2022-02-18 15:49:24 +0800

    feat: draw bingdundun

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const Select = lazy(() => import('./antd/Select'))
+const Bing = lazy(() => import('./Bing'))
 
 const App = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/select" element={<Select />} />
+        <Route path="/bing" element={<Bing />} />
       </Routes>
     </Suspense>
   </Router>
---
+++ src/Bing.tsx
@@ -0,0 +1,29 @@
+import './Bing.css'
+import logo from './assets/imgs/dongao.png'
+
+const Bing = () => (
+  <div id="bingdundun">
+    <div className="body" />
+    <div className="ear" />
+    <div className="ear right_ear" />
+    <div className="face_1" />
+    <div className="face_2" />
+    <div className="face_3" />
+    <div className="face_4" />
+    <div className="face_5" />
+    <div className="left_hand" />
+    <div className="right_hand" />
+    <div className="left_foot" />
+    <div className="right_foot" />
+    <div className="left_eye" />
+    <div className="right_eye" />
+    <div className="mouth">
+      <div className="mouth_dot" />
+    </div>
+    <div className="nose" />
+    <div className="heart" />
+    <img className="logo" src={logo} alt="logo" />
+  </div>
+)
+
+export default Bing
